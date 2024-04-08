40794456a28141f1c480db43836d6bc53883139d
Kmichaeletuk <alewo.etuk@pau.edu.ng>
2024-04-08 19:10:50 +0100

    my first commit

---
+++ Week 2/Project 2.py
@@ -0,0 +1,5 @@
+# Input from User
+A = float(input("Enter the value of A"))
+B = float(input("Enter the value of B"))
+C = float(input("Enter the value of C"))
+D = float(input("Enter the value of D"))
